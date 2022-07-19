129c5d3468f578d9098177a34f95254b103ec354
BeatrizMioranza <beatrizmioranza2508@gmail.com>
2022-07-19 11:54:14 -0300

    feat: criei o arquivo JsonParser

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String url = "https://api.mocki.io/v2/549a5d8b"; 
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request= HttpRequest.newBuilder(endereco).GET().build();  
+        HttpResponse<String> response= client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        var parser = new JsonParser();
+        List<Map<String, String>> ListaDeFilmes = parser.parse(body);
+        System.out.println(ListaDeFilmes.size());
+        System.out.println(ListaDeFilmes.get(0));
+
+        for (Map<String,String> filme : ListaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
     }
 }
